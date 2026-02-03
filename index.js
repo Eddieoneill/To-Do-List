5be7aa8efbc5e20125a2fd9692d72fa183f819f7
ejoneill <ejosephoneill@gmail.com>
2026-02-03 09:45:52 -0700

    created submit button in js file

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("my code is runnig");
+const inputDiv = document.querySelector(".inputContainer");
+const inputButton = document.createElement("button");
+const inputTextField = document.querySelector(".inputTextField");
+const lists = document.querySelector(".lists");
+let toDoList = [];
+
+inputButton.textContent = "Submit";
+inputButton.type = "button";
+
+inputButton.addEventListener("click", function () {
+  createList(inputTextField.textContent);
+  console.log(toDoList[0].textContent);
+});
+
+inputDiv.appendChild(inputButton);
+inputTextField.placeholder = "Type here...";
+
+const createList = (val) => {
+  let toDoElement = document.createElement("label");
+
+  toDoElement.textContent = val.textContent;
+  inputTextField.textContent = "";
+
+  toDoList.push(toDoElement);
+};
